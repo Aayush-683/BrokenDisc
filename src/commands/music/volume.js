c77b0a3b78701a07e2fb9cb184f322f3cc5c0b02
Dopamine <80644047+GeekHamza1@users.noreply.github.com>
2022-07-02 05:09:48 +0800

    Syntax ERROR

--- src/commands/music/volume.js
+++ src/commands/music/volume.js
@@ -26,7 +26,7 @@ module.exports = {
       return interaction.reply({ ephemeral: true, embeds: [embed] }).catch(console.error);
     }
 
-    if (!Number.isInteger(newVol) || newVol => 200 || newVol <= 0)
+    if (!Number.isInteger(newVol) || newVol >= 200 || newVol <= 0)
       return bot.say.warnMessage(interaction, "Provide a valid number between 1 to 200.");
 
     queue.setVolume(newVol);
